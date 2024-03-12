e791ed086f5e8bb5d6382a40aa6e01d77a3d0e04
PidzhakovIlya <pidzhakovilja@gmail.com>
2024-03-12 12:19:37 +0300

    BrowserRouter change on HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./App.css";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import {PostsPage} from "./pages/PostsPage/PostsPage";
 import {PostPage} from "./pages/PostPage/PostPage";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<PostsPage/>}/>
                 <Route path="/posts/:id" element={<PostPage/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
